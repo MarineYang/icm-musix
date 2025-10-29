22a0e2bd25466c90a6852158aebdd26fa08f3f90
MarineYang <zzang1725@naver.com>
2025-10-29 23:11:09 +0900

    docker version

--- migration2.sh
+++ migration2.sh
@@ -10,3 +10,6 @@ docker exec icm-musix-frontend sh -c "cat /usr/share/nginx/html/assets/*.js" | g
 
 # API Key 확인
 docker exec icm-musix-frontend sh -c "cat /usr/share/nginx/html/assets/*.js" | grep -o 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' | head -1
+
+curl -H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0" \
+  https://welcome2icm.com/supabase/rest/v1/artists
