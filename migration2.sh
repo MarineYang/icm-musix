16e5ad6b8d1b975995e381f0edb3b5c30d0a87b4
MarineYang <zzang1725@naver.com>
2025-10-29 23:03:54 +0900

    docker version

--- migration2.sh
+++ migration2.sh
@@ -1,23 +1,6 @@
+docker-compose build --no-cache \
+  --build-arg VITE_SUPABASE_URL=https://welcome2icm.com/supabase \
+  --build-arg VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0 \
+  frontend
 
-# 시스템 정보 확인
-uname -s
-uname -m
-
-# Linux x86_64인 경우 (대부분의 서버)
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
-
-# 실행 권한 부여
-sudo chmod +x /usr/local/bin/docker-compose
-
-# 기존 버전 제거 (optional)
-sudo rm /usr/bin/docker-compose
-
-# 심볼릭 링크 생성
-sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# 버전 확인
-docker-compose version
-
-# 이제 재빌드!
-docker-compose down
-docker-compose up -d --build frontend
+docker-compose up -d frontend
