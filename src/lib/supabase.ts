065b641341d27b53a1c78808812ad41e5be0d5fc
MarineYang <zzang1725@naver.com>
2025-10-30 19:27:55 +0900

    log 삭제

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 // Vite에서는 import.meta.env를 사용하여 .env 파일을 읽습니다
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'http://127.0.0.1:54321'
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
-console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 // Supabase 클라이언트 생성 (옵션 설정 포함)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
